490daa622a25ad898b336d7a3828d7c952e66bb2
raimberdiyevb <bekbolsun.raimberdiev@gmail.com>
2024-03-16 04:44:32 +0100

    modified the api-key

--- src/main/java/com/example/TaskProject/TaskProjectApplication.java
+++ src/main/java/com/example/TaskProject/TaskProjectApplication.java
@@ -1,6 +1,5 @@
 package com.example.TaskProject;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class TaskProjectApplication {
 
-	public static String API_KEY = "cfe543d2c31f2b29@m146529";
+	public static String API_KEY = "xxxxxxxxxxxxxxxxxx";
 	public static String BASE_URL = "https://api.megaventory.com/v2017a";
 	public static void main(String[] args) {
 		SpringApplication.run(TaskProjectApplication.class, args);
